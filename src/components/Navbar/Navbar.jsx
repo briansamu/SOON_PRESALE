7f0b0daf9b24f0d35ad16771a09fac065019e620
Brian Samu <brian@briansamu.com>
2023-07-21 21:51:36 -0400

    init

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import { Web3Button } from "@web3modal/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faDiscord, faYoutube } from "@fortawesome/free-brands-svg-icons"
+import './Navbar.css';
+
+const Navbar = () => {
+  return <div className="navbar">
+    <div className="navbar__container">
+      <div className="navbar__left">
+        <img className="navbar__logo" src="./assets/soonlogo.png" alt="Soon Token Logo" />
+        <nav className="navbar__nav">
+          <ul>
+            <li><a href="#!">How It Works</a></li>
+            <li><a href="#!">Tokenomics</a></li>
+            <li><a href="">Whitepaper</a></li>
+            <li><a href="">Hawaii Whitelist</a></li>
+            <li><a href="">Contact Us</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div className="navbar__right">
+        <ul>
+          <li><a href="#!" target="_blank"><FontAwesomeIcon icon={faYoutube} /></a></li>
+          <li><a href="#!" target="_blank"><FontAwesomeIcon icon={faTwitter} /></a></li>
+          <li><a href="https://discord.gg/vw6Hhu8DH7" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faDiscord} /></a></li>
+        </ul>
+        <Web3Button icon='show' label='Connect Wallet' balance='show'/>
+      </div>
+    </div>
+  </div>;
+};
+
+export default Navbar;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar/Navbar';
+
+// import { Web3Button } from "@web3modal/react";
+
+const HomePage = () => {
+  return <>
+    <Navbar />
+    {/* <Web3Button icon='show' label='Connect Wallet' balance='show'/> */}
+  </>;
+};
+
+export default HomePage;
