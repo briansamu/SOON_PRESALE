7f163e7bc8e90d4d0b061994198f8a1b31be41d5
Brian Samu <brian@briansamu.com>
2023-07-24 16:22:19 -0400

    added presale component

---
+++ src/components/Presale/Presale.jsx
@@ -0,0 +1,11 @@
+import './Presale.css';
+
+const Presale = () => {
+  return <div className="presale">
+    <div className="presale__container">
+        <h1>Presale Section</h1>
+    </div>
+  </div>;
+};
+
+export default Presale;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import Navbar from '../components/Navbar/Navbar';
+import Presale from '../components/Presale/Presale';
 
 // import { Web3Button } from "@web3modal/react";
 
 const HomePage = () => {
   return <>
     <Navbar />
+    <Presale />
     {/* <Web3Button icon='show' label='Connect Wallet' balance='show'/> */}
   </>;
 };
