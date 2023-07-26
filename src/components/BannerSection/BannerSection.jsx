b5e37d1a25f5bd9520c9d401779ed2b26fbdcb39
Brian Samu <brian@briansamu.com>
2023-07-26 09:55:51 -0400

    added content to bannersection and whitepapersection. need to make responsive

--- src/components/BannerSection/BannerSection.jsx
+++ src/components/BannerSection/BannerSection.jsx
@@ -2,7 +2,7 @@ import './BannerSection.css';
 
 const BannerSection = () => {
     return <div className='bannersection'>
-        
+        <img className='bannersection-img' src="./assets/soongraphic.png" alt="soon graphic" />
     </div>
 }
 
--- src/components/WhitepaperSection/WhitepaperSection.jsx
+++ src/components/WhitepaperSection/WhitepaperSection.jsx
@@ -1,7 +1,12 @@
 import './WhitepaperSection.css';
 
 const WhitepaperSection = () => {
-    return <div className='whitepapersection'></div>
+    return <div className='whitepapersection'>
+      <div className="whitepapersection__container">
+        <h3 id="whitepaperh3">Read Our White Paper</h3>
+        <button className="whitepaper-btn">Whitepaper</button>
+      </div>
+    </div>
 }
 
 export default WhitepaperSection
