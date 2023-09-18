a7c80848a629f37c19cabd0110ec01fbddc2a899
Brian Samu <brian@briansamu.com>
2023-09-18 09:51:26 -0400

    changed pepe image to gif

--- src/components/PresaleSection/PresaleSection.jsx
+++ src/components/PresaleSection/PresaleSection.jsx
@@ -6,15 +6,15 @@ const Presale = () => {
     <div className="presale__container">
         <div className="presale__left">
           <PresaleForm />
-          <img className='studio-img' src="../../assets/52studios.jpg" alt='Image' />
+          <img className='studio-img' src="../../assets/52studios.jpg" alt='pepe coming soon' />
         </div>
         <div className="presale__right">
-          <img src="../../assets/morecomingsoon.png" alt="Image" />
+          <img src="../../assets/pepecomingsoon.gif" alt="Image" />
           <div className="presale__right-textwrapper">
             <h3>Web Series Coming Soon</h3>
             <p>Welcome to &quot;<strong>Soon</strong>&quot;, where we&apos;re going to dive into the creation of an eccentric, eight-episode animated web series that&apos;s brimming with slapstick humor. The star of our series is a mythical being from the MEME Realm known as <strong>WEN</strong>. Along his journey, <strong>WEN</strong> will encounter other meme world dwellers like <strong>DOGE, SHIBA INU, PEPE</strong>, and beyond! As a <strong>$Soon</strong> node holder, you&apos;ll reap various benefits while also indulding in a cryptocurrency-themed, meme-inspired animated series. So, when you question &quot;<strong>wen</strong>&quot;, our answer is always &quot;<strong>Soon</strong>&quot;.</p>
           </div>
-          <img src="../../assets/soon.gif" alt="Image" />
+          <img src="../../assets/soon.gif" alt="soon tm" />
         </div>
     </div>
   </div>;
