5ac81523f1f59b257aecaba4d82826b49556bc77
CryptoDev777 <95718885+Blockchain1207@users.noreply.github.com>
2023-09-19 01:22:47 +0900

    web3 integration
    integrate with web3modal and wagmi for testnet

---
+++ src/components/PresaleForm/config.js
@@ -0,0 +1,9 @@
+export const usdcDecimal = 6
+export const tokenDecimal = 18
+
+export const ETHChainId = 5
+
+export const usdcAddress = "0x012465E877958E01111fE9baF96C27eb401FA142"
+export const tokenAddress = "0x77DC72FfCBdCa56020923e1A1e6d5F5AEc799168"
+export const presaleAddress = "0x5Ab0787a7Fc9E0E346794cd33a697D8966345A3e"
+export const fundAddress = "0x7Ad696FC88B9Cc87c138859F0623872feFa08F56"
