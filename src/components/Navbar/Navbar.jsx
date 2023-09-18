e5dfe34a4ebd5b0960f53416c329c4eee84503f4
Brian Samu <brian@briansamu.com>
2023-09-18 18:23:00 -0400

    added htaccess and removed #! from nav links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,8 +18,8 @@ const Navbar = () => {
         <nav ref={navRef} className="navbar__nav">
           <button onClick={showNavbar} className="navbar__close"><FontAwesomeIcon icon={faX} /></button>
           <ul>
-            <li><a href="#!">How It Works</a></li>
-            <li><a href="#!">Tokenomics</a></li>
+            <li><a href="#">How It Works</a></li>
+            <li><a href="#">Tokenomics</a></li>
             <li><a href="">Whitepaper</a></li>
             <li><a href="">Hawaii Whitelist</a></li>
             <li><a href="">Contact Us</a></li>
@@ -29,8 +29,8 @@ const Navbar = () => {
       </div>
       <div className="navbar__right">
         <ul>
-          <li><a href="#!" target="_blank"><FontAwesomeIcon icon={faYoutube} /></a></li>
-          <li><a href="#!" target="_blank"><FontAwesomeIcon icon={faTwitter} /></a></li>
+          <li><a href="#" target="_blank"><FontAwesomeIcon icon={faYoutube} /></a></li>
+          <li><a href="#" target="_blank"><FontAwesomeIcon icon={faTwitter} /></a></li>
           <li><a href="https://discord.gg/vw6Hhu8DH7" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faDiscord} /></a></li>
         </ul>
       </div>
