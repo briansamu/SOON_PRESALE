1e55c179c5bb414dfa5a0acaa5e4713c21fbc475
Brian Samu <brian@briansamu.com>
2023-09-21 10:26:03 -0400

    added new whitepaper link

--- src/components/WhitepaperSection/WhitepaperSection.jsx
+++ src/components/WhitepaperSection/WhitepaperSection.jsx
@@ -4,7 +4,7 @@ const WhitepaperSection = () => {
     return <div className='whitepapersection'>
       <div className="whitepapersection__container">
         <h3 id="whitepaperh3">Read Our White Paper</h3>
-        <button className="whitepaper-btn"><a href="https://cdn.discordapp.com/attachments/1111826017754042458/1153461911124451408/Whitepaper.pdf">Whitepaper</a></button>
+        <button className="whitepaper-btn"><a href="https://wen.finance/whitepaper.pdf">Whitepaper</a></button>
       </div>
     </div>
 }
