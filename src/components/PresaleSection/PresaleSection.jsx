11359434868febe209c61736282495ce3b93fe2f
Brian Samu <brian@briansamu.com>
2023-09-25 10:56:39 -0400

    changed soon gif to be smaller

--- src/components/PresaleSection/PresaleSection.jsx
+++ src/components/PresaleSection/PresaleSection.jsx
@@ -14,7 +14,7 @@ const Presale = () => {
             <h3>Web Series Coming Soon</h3>
             <p>Welcome to "Soon", where we're going to dive into the creation of an eccentric, eight-episode pilot animated web series that's brimming with slapstick humor.  Based on a TRUE events, this will be the greatest come up story ever told. The star of our series is a mythical being from the MEME Realm known as WEN. Along his journey, WEN will encounter other meme world dwellers like DOGE, SHIBA INU, PEPE, and beyond! As a $Soon node holder, you'll reap various benefits while also indulging in a cryptocurrency-themed, meme-inspired animated series. So, when you question "wen", our answer is always "Soon".</p>
           </div>
-          <img src="../../assets/soon.gif" alt="soon tm" />
+          <img id='soongif' src="../../assets/soon.gif" alt="soon tm" />
         </div>
     </div>
   </div>;
